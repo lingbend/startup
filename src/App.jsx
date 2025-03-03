acc69835d1285d88d25d847e3378481fcd20cf53
lingbend <lingbend@gmail.com>
2025-03-03 12:28:36 -0700

    Created app placeholder

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '/src/main.css';
+
+export default function App() {
+    return (
+        <>
+        <div>Placeholder</div>
+        </>
+    );
+}
