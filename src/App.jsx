0e6d55cdab60d0f4bfaf800b65949d07a5ff6323
lingbend <lingbend@gmail.com>
2025-03-03 13:02:24 -0700

    Added header and footer to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,23 @@ import '/src/main.css';
 
 export default function App() {
     return (
-        <>
+        <div className="bg-dark text-light container-fluid" style={{"padding":"0px"}}>
+        <header>
+            <h1 className="title">Goals Gamified
+                <img src="/flameIcon.png" alt="" height="32"/>
+            </h1>
+            <nav>
+                <menu className="btn-group">
+                    <li><a className="btn btn-outline-light m-4" style={{"min-width":"110px"}} href="/index.html">Login page</a></li>
+                    <li><a className="btn btn-outline-light m-4" style={{"min-width":"110px"}} href="/goals.html">My Goals</a></li>
+                </menu>
+            </nav>
+        </header>
         <div>Placeholder</div>
-        </>
+        <footer>
+            <h2>Website by: Benjamin Lingwall</h2>
+            <a href="https://github.com/lingbend/startup" className="link-info" target="_blank">GitHub</a>
+        </footer>
+        </div>
     );
 }
