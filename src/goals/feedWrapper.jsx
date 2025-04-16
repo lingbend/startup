85c4bceb8c6079e35b4d972eacad096ade914aee
lingbend <lingbend@gmail.com>
2025-04-16 11:50:08 -0600

    Drafted feedWrapper webSocket receiving

--- src/goals/feedWrapper.jsx
+++ src/goals/feedWrapper.jsx
@@ -13,11 +13,11 @@ export function FeedWrapper(){
         if (feedList == null) {
             setFeedList(() => [{icon:"bi bi-hand-thumbs-up", text:"Suzie decided to eat styrofoam!", visible:false}]);
         }
-        setInterval(() => {
-            let fillerList = [{icon:"bi bi-hand-thumbs-up", text:"Suzie decided to eat styrofoam!", visible:false}, 
-                {icon:"bi bi-check2-circle", text:"Johnny planted an appleseed!", visible:false}
-            ];
-            let temp = fillerList.at(Math.floor(Math.random()*2));
+        let port = window.location.port;
+        let protocol = window.location.protocol == "https" ? "wss" : "ws";
+        let webSocket = new WebSocket(protocol + "://" + window.location.host + ":" + port + "/ws");
+        webSocket.onmessage(async (message) => {
+            let temp = await JSON.parse(message);
             setFeedList((feedList) => {
                 if (feedList == null) {
                     return [temp];
@@ -28,8 +28,27 @@ export function FeedWrapper(){
                 let newFeedList = (structuredClone(feedList))
                 newFeedList.push(temp);
                 return newFeedList;
-            });
-        }, 2000);
+            })
+            
+        })
+
+        // setInterval(() => {
+        //     let fillerList = [{icon:"bi bi-hand-thumbs-up", text:"Suzie decided to eat styrofoam!", visible:false}, 
+        //         {icon:"bi bi-check2-circle", text:"Johnny planted an appleseed!", visible:false}
+        //     ];
+        //     let temp = fillerList.at(Math.floor(Math.random()*2));
+        //     setFeedList((feedList) => {
+        //         if (feedList == null) {
+        //             return [temp];
+        //         }
+        //         if (feedList.length > 5) {
+        //             feedList.shift();
+        //         }
+        //         let newFeedList = (structuredClone(feedList))
+        //         newFeedList.push(temp);
+        //         return newFeedList;
+        //     });
+        // }, 2000);
     },[])
     return (<Fragment>
             <FeedItem num={0} feedList={feedList}/>
